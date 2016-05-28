85869758ff98454afdd5574fb4c51d3a754ce80b
andy3d <andylee3d@gmail.com>
2016-05-28 17:52:44 +0900

    계산기

---
+++ app/src/main/java/com/example/user/mycalc/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.user.mycalc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    EditText etName1,etName2;
+    Button btn1,btn2,btn3,btn4,btn5;
+    TextView tvName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("=초간단계산기=");
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+        etName1 =(EditText) findViewById(R.id.etName1);
+        etName2 =(EditText) findViewById(R.id.etName2);
+        tvName =(TextView) findViewById(R.id.tvName);
+
+    }
+}
